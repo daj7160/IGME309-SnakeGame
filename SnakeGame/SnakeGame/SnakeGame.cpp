891ad9672643913913a2d00e808b9e1d79ef5173
Darius James <daj7160@g.rit.edu>
2019-11-14 17:53:24 -0500

    Window drawing, workign on adding square

---
+++ SnakeGame/SnakeGame/SnakeGame.cpp
@@ -0,0 +1,61 @@
+// SnakeGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "SnakeGame.h"
+using namespace glm;
+
+int main()
+{
+	// Initialise GLFW
+	glfwInit();
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Snake", NULL, NULL);
+	unsigned int id = 0;
+	glGenBuffers(1, &id);
+
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.5f,  0.5f, 0.0f
+	 -0.5f, -0.5f, 0.0f
+	};
+
+	
+	if (window == NULL) {
+		std::cout << "Could not create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glViewport(0, 0, 1024, 768);
+
+	glBindBuffer(GL_ARRAY_BUFFER, id);
+	glBufferData(GL_ARRAY_BUFFER, (sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ SnakeGame/SnakeGame/SnakeGame.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef _SNAKEGAME_H 
+#define _SNAKEGAME_H 
+
+#include <iostream> 
+#include <stdlib.h>
+
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+#include "GL\glut.h"
+
+#include "GLM\glm.hpp"
+#include "OpenGL-Tutorials\shader.hpp"
+
+#endif //_SNAKEGAME_H 
+
+
